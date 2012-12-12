c00364c2fec5327916a7737304588cf39a57b1e0
Raja hafify <rajahafify.zaed@gmail.com>
2012-12-12 21:47:08 +0800

    pushing static app to heroku

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<?//php include_once('/includes/init.php'); ?>
-<?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  // we will fill this out next
-}
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-  echo "Database connection error."
-  exit;
-}
-$result = pg_query($db, "SELECT statement goes here");
-?>
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Giving Joy</title>
@@ -148,4 +133,3 @@ $result = pg_query($db, "SELECT statement goes here");
 </body>
 
 </html>
- -->
