b8ddd359395a7605d6046d72a336ba5f0a93cc7f
Ash Menon <ash@ashmenon.com>
2012-12-21 17:10:08 +0800

    Moved PayPal details to init.php file. Added favicon.

--- includes/init.php
+++ includes/init.php
@@ -1,5 +1,5 @@
 <?php
-include_once('/includes/functions.php');
+include_once('./includes/functions.php');
 $db_host = 'localhost';
 $db_name = 'givingjoydb';
 $db_user = 'givingjoyuser';
@@ -8,9 +8,21 @@ $db_pass = '5w*29e1gef1#$#u@';
 $con = mysql_connect($db_host,$db_user,$db_pass) or die('Error connecting to database');
 $db = mysql_select_db($db_name,$con) or die('Error selecting database.');
 
-
+//Email name and address for the server.
 $adminemail = 'info@givingjoy.org';
 $adminname = 'GivingJoy.org';
+
+
+//PayPal details.
+$merchant_username = 'shahtr_1354930404_biz_api1.gmail.com';
+$merchant_password = '1354930427';
+$merchant_signature = 'AFcWxV21C7fd0v3bYYYRCpSSRl31A1mqIs4dspFHemQXZlaEsLVRfCVh';
+$sandbox = true;
+$currency = 'USD';
+$version = 93;
+
+
+$domain = 'http://local.givingjoy.org';
 
 session_start();
 
